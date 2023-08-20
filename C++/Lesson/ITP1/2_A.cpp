ddde281a1782ea9ed2f2be64048363f14e285d1c
hiro1729 <93374538+hiro1729@users.noreply.github.com>
2023-08-20 13:06:52 +0900

    a

---
+++ C++/Lesson/ITP1/2_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a < b) {
+        cout << a << " < " << b << '\n';
+    } else if (a > b) {
+        cout << a << " > " << b << '\n';
+    } else {
+        cout << a << " == " << b << '\n';
+    }
+}
